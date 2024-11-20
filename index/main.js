4a8b0fb90e539afa1d20c96d538f6976efe3bbb9
Yuri Hatakeyama <yuri.min.hatakeyama@gmail.com>
2024-11-19 18:26:12 -0600

    Added photos from photoshoot. Overlay in discography. Beginning footer section. Etc.

--- index/main.js
+++ index/main.js
@@ -4,3 +4,19 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.height = "0%";
 }
+
+
+function openExitRight() {
+    document.getElementById("myExitRight").style.display = "block";
+}
+function closeExitRight() {
+    document.getElementById("myExitRight").style.display = "none";
+}
+
+function openConquer() {
+    document.getElementById("myConquer").style.display = "block";
+}
+function closeConquer() {
+    document.getElementById("myConquer").style.display = "none";
+}
+
